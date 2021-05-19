126ba900636265f08868b5c45bbc5061ee552eba
Miguel Angel Posada Ibañez <64515605+Posi-10@users.noreply.github.com>
2021-05-19 09:16:48 -0500

    Initial commit

---
+++ app/configuracion.php
@@ -0,0 +1,5 @@
+<?php
+  define("NOMBRE_SERVIDOR", "posiconexion");
+  define("SERVIDOR", "http://localhost/SistemasWEB/boton/");
+  //define("SERVIDOR", "http://localhost/boton/");
+?>
---
+++ view/inicio.php
@@ -0,0 +1,11 @@
+<div class="container">
+  <div class="row mt-5">
+    <div class="col mt-5">
+      <span class="btn btn-outline-info btn-block btn-xl" id="btn_enviar">Eviar</span>
+    </div>
+    <div class="col mt-5">
+      <input type="text" id="mensaje" readonly></input>
+    </div>
+  </div>
+</div>
+<script src="manager/manager_inicio.js"></script>
